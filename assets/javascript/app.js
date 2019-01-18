53cc0b7b282c05c000700c1d20f07471a1df19bd
rkpeltier <rkpeltier@gmail.com>
2019-01-17 23:34:44 -0800

    added some notes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-
-
-
-
+//Add functionality to start button that when clicked will display questions and start timer
+//Select single answer for questions and point to correct div
+//Add condition to when timer runs out the game is over and displays the score screen
+//Score screen includes: right asnwers, wrong answers, and unanswered
 
 
 });
