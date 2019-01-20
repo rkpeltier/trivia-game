a9012102a13ea0a30ea50a3fb9ae1653635e1f01
rkpeltier <rkpeltier@gmail.com>
2019-01-20 13:33:19 -0800

    fixed countdown timer so it works now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,23 @@
-$(document).ready(function() {
-
 //Variables
 var correctAnswer = 0;
 var wrongAnser = 0;
 var noAnswer = 0;
 var timer = 60;
+var intervalId;
+
 
 //Start Button
-$("#start").on("click", function() {
-    $(this).hide();
-    timer = setInterval(1000);
-    $("#count-down").html(timer);
+$("#start").on("click", function(){
+    startClock();
 });
 
-//Trivia Questions
-var triviaQuestion = [{
-    question: "What is the name of a camera move that combines a dolly and a zoom?",
-    multipleChoice: ["Pan", "Panovision", "Zolly", "Tracking"],
-    answer: [2]
-}, {
-
+//Timer function
+function startClock() {
+    intervalId = setInterval(decrement, 1000);
 }
 
-];
-
-//Some Functions
-function displayTrivia() {
-    $("#questions").append(triviaQuestion);
+function decrement() {
+    timer--;
+    $("#count-down").html("Time Remaining: " + timer); 
 }
 
-displayTrivia();
-
-});
