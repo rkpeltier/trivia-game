da57a15ffd8680a248f50cc176793fe797f9838b
rkpeltier <rkpeltier@gmail.com>
2019-01-19 16:46:27 -0800

    Updated jquery and html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
 
+//Variables
+var correctAnswer = 0;
+var wrongAnser = 0;
+var noAnswer = 0;
+
 //Start Button
-$("#start").on("Click", function() {
+$("#start").on("click", function() {
+    $(this).hide();
     
 });
+
+//Trivia Questions
+var triviaQuestion = [{
+    question: "What is the name of a camera move that combines a dolly and a zoom?",
+    multipleChoice: ["Pan", "Panovision", "Zolly", "Tracking"],
+    answer: [2]
+}, {
+    
+}
+
+];
 
 
 });
